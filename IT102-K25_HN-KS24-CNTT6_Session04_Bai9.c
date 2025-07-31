99d598c1aaaccae0340a084be6d61ac807bf8931
TranQuoc2k6 <qatran2k6@gmail.com>
2025-07-31 08:07:52 +0700

    Update IT102-K25_HN-KS24-CNTT6_Session04_Bai9.c

--- IT102-K25_HN-KS24-CNTT6_Session04_Bai9.c
+++ IT102-K25_HN-KS24-CNTT6_Session04_Bai9.c
@@ -11,31 +11,40 @@ int main() {
     printf("Nhap nam:");
     scanf("%d", &year);
 
-    switch (month) {
-        case 1: case 3: case 5: case 7: case 8: case 10: case 12: {
-            if (day >31 || day < 1) {
-                printf("Ngay khong hop le");
-            }else {
-                printf("%d/%d/%d", day, month, year);
+    if (year > 0) {
+        if (month > 0 && month <= 12) {
+            switch (month) {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    if (day > 0 && day <= 31) {
+                        printf("%d/%d/%d", day, month, year);
+                    }else {
+                        printf("Ngay thang nam khong hop le 1");
+                    }
+                    break;
+                case 4: case 6: case 9: case 11: case 13:
+                    if (day > 0 && day <= 30) {
+                        printf("%d/%d/%d", day, month, year);
+                    }else {
+                        printf("Ngay thang nam khong hop le 2");
+                    }
+                    break;
+                case 2:
+                    if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+                        if (day > 0 && day <= 29) {
+                            printf("%d/%d/%d", day, month, year);
+                        }
+                    }else {
+                        if (day > 0 && day <= 28) {
+                            printf("%d/%d/%d", day, month, year);
+                        }
+                    }
+                    break;
             }
-            break;
+        }else {
+            printf("Ngay thang nam khong hop le 3");
         }
-        case 4: case 6: case 9: case 11: {
-            if (day > 30 || day < 1) {
-                printf("Ngay khong hop le");
-            }else {
-                printf("%d/%d/%d", day, month, year);
-            }
-            break;
-        }
-        case 2: {
-            if (day > 29 || day < 1) {
-                printf("Ngay khong hop le");
-            }else {
-                printf("%d/%d/%d", day, month, year);
-            }
-            break;
-        }
+    }else {
+        printf("Ngay thang nam khong hop le 4");
     }
     return 0;
 }
